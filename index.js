f0a17a769927612a24526231ac05444729445f4c
Bilha <bilha.leposo@student.moringaschool.com>
2024-03-27 06:24:43 +0300

    complete functions continued

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return "This Saturday, I want to ${activity}!";
+}
+saturdayFun();
+
+//Call the function with the default argument
+function saturdayFun (activity = "roller-skate") {
+    return (This Saturday, I want to ${activity}!);
+}
+
+
+//Call the function with the default argument
+function mondayWork (activity ="go to the office") {
+    return (This Monday, I will ${activity}.);
+}
+
+function wrapAdjective (symbol = '*') {
+    return function (adjective){
+        return You are ${symbol}${adjective}${symbol}! ;
+    }
+}
